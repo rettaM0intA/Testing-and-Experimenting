d21ec93a3aa5ccab9191e64b35eef5c4dd43b2be
rettaM0intA <vansnepsonc23@gmail.com>
2023-07-03 15:02:04 -0400

    Fixed update button

---
+++ Todo/Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using Todo.Models;
+using Todo.Models.ViewModels;
+
+namespace Todo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var todoListViewModel = GetAllTodos();
+        return View(todoListViewModel);
+    }
+
+    
+    [HttpGet]
+    public JsonResult PopulateForm(int id){
+        var todo = GetById(id);
+        return  Json(todo);
+    }
+
+    public TodoViewModel GetAllTodos(){
+        List<TodoItem> todoList = new();
+
+        using (SqliteConnection con =
+            new SqliteConnection("Data Source=db.sqlite")){
+            using (var tableCmd = con.CreateCommand()){
+                con.Open();
+                tableCmd.CommandText = "SELECT * FROM todo";
+            
+                using (var reader = tableCmd.ExecuteReader()){
+                    if(reader.HasRows){
+                        while(reader.Read()){
+                            todoList.Add(
+                                new TodoItem
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1)
+                                }
+                            );
+                        }
+                    }
+                    else{
+                        return new TodoViewModel{
+                            TodoList = todoList
+                        };
+                    }
+                }
+            }
+        }
+        return new TodoViewModel{
+            TodoList = todoList
+        };
+    }
+
+    public RedirectResult Insert(TodoItem todo){
+        using (SqliteConnection con =
+            new SqliteConnection("Data Source=db.sqlite")){
+
+            using(var tableCmd  = con.CreateCommand()){
+                con.Open();
+                tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                try{
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch(Exception ex){
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return Redirect("http://localhost:5271/");
+        }
+    }
+
+    public JsonResult Delete(int id){
+        using(SqliteConnection con =
+            new SqliteConnection("Data Source=db.sqlite")){
+            using (var tableCmd = con.CreateCommand()){
+                con.Open();
+                tableCmd.CommandText=$"DELETE from todo WHERE id = '{id}'";
+                tableCmd.ExecuteNonQuery();
+            }
+        }
+
+        return Json(new {});
+        
+    }
+
+
+    public TodoItem GetById(int id){
+        TodoItem todo = new();
+
+        using (var connection = 
+            new SqliteConnection("Data Source=db.sqlite")){
+            using(var tableCmd = connection.CreateCommand()){
+                connection.Open();
+                tableCmd.CommandText = $"SELECT * FROM todo WHERE Id = '{id}'";
+
+                using (var reader = tableCmd.ExecuteReader()){
+                    if(reader.HasRows){
+                        reader.Read();
+                        todo.Id = reader.GetInt32(0);
+                        todo.Name = reader.GetString(1);
+                    }
+                }
+            }
+        }
+        return todo;
+    }
+
+    public RedirectResult Update(TodoItem todo){
+        using (SqliteConnection con =
+            new SqliteConnection("Data Source=db.sqlite")){
+            using (var tableCmd = con.CreateCommand()){
+                con.Open();
+                tableCmd.CommandText = $"UPDATE todo SET name = '{todo.Name}' WHERE Id = '{todo.Id}'";
+                try{
+                    tableCmd.ExecuteNonQuery();
+                }
+                catch (Exception ex){
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        return Redirect("http://localhost:5271/");
+    }
+}
+
